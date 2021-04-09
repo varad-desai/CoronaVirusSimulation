a8f0794d2e45a4518bdb747fb12bacbb2a3660f6
Jaeline Granda <granda.j@northeastern.edu>
2021-04-08 21:55:27 -0400

    Implemented covid testing to show effects of virus spread

--- coronaVirusSimulation1/src/main/java/GuiCovidTesting/AnimationPanel.java
+++ coronaVirusSimulation1/src/main/java/GuiCovidTesting/AnimationPanel.java
@@ -13,7 +13,7 @@ public class AnimationPanel extends JPanel implements ActionListener {
     // Timer is needed for animation
    private Timer tm = new Timer(100, this);
    // the number of people in the simulation
-   private int numPoints = 200;
+   private int numPoints = 1000;
    // an array of Person objects
    private Person[] p = new Person[numPoints];
    // how large to draw each Person
@@ -47,7 +47,7 @@ public class AnimationPanel extends JPanel implements ActionListener {
 //      System.out.println(p.length);
        for(int i=0;i<p.length;i++) {
        	p[i].testing();
-       	if (!p[i].testResult) {	
+       	if (!p[i].testResult && !p[i].died) {	
            p[i].move();
        	}
            p[i].checkForImmunity();
@@ -60,6 +60,7 @@ public class AnimationPanel extends JPanel implements ActionListener {
               " Infected: "+calculate_no_of_infected()+
               " Immune: "+calculate_no_of_immune()+
               " Susceptible: "+calculate_no_of_susceptible()+
+              " Deaths: "+calculate_no_of_deaths()+
               " Population: "+p.length
               );
       repaint();
@@ -94,7 +95,8 @@ public class AnimationPanel extends JPanel implements ActionListener {
        for(int i=0; i<p.length; i++){
            if(p[i].immune) no_of_immune++;
        }
-       return no_of_immune;
+       int deaths = calculate_no_of_deaths();
+       return no_of_immune - deaths;
    }
    
    public int calculate_no_of_susceptible(){
@@ -103,6 +105,14 @@ public class AnimationPanel extends JPanel implements ActionListener {
            if(p[i].infected == 0) no_of_susceptible++;
        }
        return no_of_susceptible;
+   }
+   
+   public int calculate_no_of_deaths() {
+       int no_of_deaths = 0;
+       for(int i=0; i<p.length; i++){
+           if(p[i].died) no_of_deaths++;
+       }
+       return no_of_deaths;
    }
    
    public void handleCollisions() {
@@ -116,18 +126,18 @@ public class AnimationPanel extends JPanel implements ActionListener {
             // People who tested positive are assumed to quarantine for 2 weeks
             // This slows down the spread
             boolean allowed_to_get_infected;
-            int random_number = gen.nextInt(101);
-            allowed_to_get_infected = random_number <= 8;
+            int random_number = gen.nextInt(100);
+            allowed_to_get_infected = random_number >= 1 && random_number <= 8;
             // if the distance between 2 points is small enough, and one of
             // the Persons is infected, then infect the other Person
             if (dist < infectDistance) {
-               if (p[i].infected > 0 && !p[i].immune && !p[j].immune && p[j].infected == 0) {
+               if (p[i].infected > 0 && !p[i].immune && !p[j].immune && !p[i].died && p[j].infected == 0) {
             	   if(allowed_to_get_infected){
                        p[j].infected++;
                        p[i].no_of_person_infected++;
                    }
                }
-               if (p[j].infected > 0 && !p[j].immune && !p[i].immune && p[i].infected == 0) {
+               if (p[j].infected > 0 && !p[j].immune && !p[i].immune && !p[j].died && p[i].infected == 0) {
                    if(allowed_to_get_infected){
                 	   p[i].infected++;
                 	   p[j].no_of_person_infected++;
@@ -151,10 +161,13 @@ public class AnimationPanel extends JPanel implements ActionListener {
         		 g.setColor(Color.red);
         	 }
          }
+         	else if (p[i].died) {
+        	g.setColor(Color.black);
+         }
          	else if (p[i].immune) {
             g.setColor(Color.green);
          } else {
-            g.setColor(Color.black);
+            g.setColor(Color.blue);
          }
          g.fillOval(p[i].x, p[i].y, circleSize, circleSize);
       }
